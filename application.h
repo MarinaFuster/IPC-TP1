cc73590f7105bd1c3c6b4f96a2a4efd9962ba046
Marina Fuster <37815201+MarinaFuster@users.noreply.github.com>
2019-04-07 15:46:41 -0300

    Add files via upload

---
+++ application.h
@@ -0,0 +1,14 @@
+#ifndef APPLICATION_H
+
+#define EXIT_ERROR 1
+#define SLAVEQ 5
+#define FILES_PER_SLAVE 10
+#define SEPARATOR '\0'
+#define SHARED_MEMORY_SIZE 50*150 // Max 150 files to process
+#define SELF_PATH "./application"
+#define SLAVE_PATH "./slave"
+#define MD5_PATH "/usr/bin/md5sum"
+
+#define MAX_FILE_LENGTH 50
+
+#endif // APPLICATION_H
