bcf73fa8291af0655fa222e3b5ce520c8a7ce7b9
Marina <mfuster@itba.edu.ar>
2019-04-08 17:27:34 -0300

    Order defines alphabetically

--- application.h
+++ application.h
@@ -1,14 +1,14 @@
 #ifndef APPLICATION_H
 
 #define EXIT_ERROR 1
-#define SLAVEQ 5
 #define FILES_PER_SLAVE 10
-#define SEPARATOR '\0'
-#define SHARED_MEMORY_SIZE 50*150 // Max 150 files to process
-#define SELF_PATH "./application"
-#define SLAVE_PATH "./slave"
 #define MD5_PATH "/usr/bin/md5sum"
-#define TOLERANCE 25
 #define MAX_FILE_LENGTH 50
+#define SELF_PATH "./application"
+#define SEPARATOR '\0'
+#define SHARED_MEMORY_SIZE 90*150 // 90 equals to max length of <hash : file_name > , 150 max amount of files
+#define SLAVE_PATH "./slave"
+#define SLAVEQ 5
+#define TOLERANCE 25 // If file quantity >= tolerance, files are released in a batch
 
 #endif // APPLICATION_H
